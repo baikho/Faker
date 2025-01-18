ed7cb6bb7ad3e188383428936f66e89828370d75
Tommaso Randazzo <55696-tommasorandazzo@users.noreply.drupalcode.org>
2025-01-18 23:56:25 +0000

    Issue #3297171 by tommasorandazzo, project update bot, baikho, jmaxant: Automated Drupal 10 compatibility fixes

--- src/Plugin/FakerDataSampler/ImageFakerDataSampler.php
+++ src/Plugin/FakerDataSampler/ImageFakerDataSampler.php
@@ -57,7 +57,7 @@ class ImageFakerDataSampler extends FakerDataSamplerBase {
       $destination_dir = static::doGetUploadLocation($settings);
       $file_system->prepareDirectory($destination_dir, FileSystemInterface::CREATE_DIRECTORY);
       $destination = $destination_dir . '/' . basename($path);
-      $file = file_move($image, $destination);
+      $file = \Drupal::service('file.repository')->move($image, $destination);
       $images[$extension][$min_resolution][$max_resolution][$file->id()] = $file;
     }
     else {
